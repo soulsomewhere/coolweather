56569cd68ec84330124c2e3f52ef55597aaaad9e
miaosen <miaosen@hust.edu.cn>
2016-05-03 23:01:39 +0800

    完成遍历

---
+++ src/com/collweather/app/activity/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.collweather.app.activity;
+
+import com.collweather.app.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherActivity extends Activity {
+ @Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.weather_layout);
+}
+}
